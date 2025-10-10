93cc578ecf27b186fa8e9f851cb1b08eeadf9e42
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-10 20:28:36 +0530

    feat(welcomeScreen):Create welcome screen UI

--- src/com/pcl/lms/controller/WelcomeScreenFormController.java
+++ src/com/pcl/lms/controller/WelcomeScreenFormController.java
@@ -1,4 +1,17 @@
 package com.pcl.lms.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class WelcomeScreenFormController {
+    public AnchorPane context;
+    public Label lblCompany;
+    public Label lblVersion;
+
+    public void navigateLoginFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void navigateSignupFormOnAction(ActionEvent actionEvent) {
+    }
 }
