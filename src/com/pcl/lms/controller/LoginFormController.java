1daeb9054d800bfa127b36a0a31a19800de684d6
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-10 21:15:25 +0530

    feat(loginform):create login form UI

--- src/com/pcl/lms/controller/LoginFormController.java
+++ src/com/pcl/lms/controller/LoginFormController.java
@@ -1,4 +1,30 @@
 package com.pcl.lms.controller;
 
+import com.pcl.lms.env.StaticResource;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public Label lblCompany;
+    public Label lblVersion;
+
+    public void initialize() {
+        setStaticData();
+    }
+
+    private void setStaticData() {
+        lblCompany.setText(StaticResource.getCOMPANY());
+        lblVersion.setText(StaticResource.getVERSION());
+    }
+
+
+
+    public void navigateDashboardOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void navigateForgotPasswordOnAction(ActionEvent actionEvent) {
+    }
 }
