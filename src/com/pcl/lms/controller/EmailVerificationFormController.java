56124aa1220ea0cfd6a7877f14e1e3683039e265
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-10 21:35:58 +0530

    refactor(login nav):implement setui for avoid boilerplationg

---
+++ src/com/pcl/lms/controller/EmailVerificationFormController.java
@@ -0,0 +1,11 @@
+package com.pcl.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class EmailVerificationFormController {
+    public AnchorPane context;
+
+    public void navigateVerifyCodeOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/com/pcl/lms/controller/LoginFormController.java
+++ src/com/pcl/lms/controller/LoginFormController.java
@@ -2,8 +2,15 @@ package com.pcl.lms.controller;
 
 import com.pcl.lms.env.StaticResource;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -25,6 +32,20 @@ public class LoginFormController {
 
     }
 
-    public void navigateForgotPasswordOnAction(ActionEvent actionEvent) {
+    public void navigateForgotPasswordOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("EmailVerificationForm");
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("WelcomScreenForm");
+
+    }
+    private void setUi(String location) throws IOException {
+        URL resource = getClass().getResource("/com/pcl/lms/view/"+location+".fxml");
+
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        Stage stage= (Stage) context.getScene().getWindow();
+        stage.setScene(scene);
     }
 }
