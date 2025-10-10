65955e4a83e3bbe82dee1bed350a738ef32cb394
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-10 22:45:35 +0530

    feat(signup):implement signupform navigations

--- src/com/pcl/lms/controller/SignupFormController.java
+++ src/com/pcl/lms/controller/SignupFormController.java
@@ -1,14 +1,25 @@
 package com.pcl.lms.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupFormController {
     public AnchorPane context;
 
-    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
     }
 
-    public void signUpOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+    private void setUi(String location) throws IOException {
+        Stage stage =(Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/pcl/lms/view/"+location+".fxml"))));
     }
 }
