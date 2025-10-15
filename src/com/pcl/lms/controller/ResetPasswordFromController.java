c8e60f6d0f6194d8fcb9c8acbfde776bb158ea15
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-15 22:13:55 +0530

    feat(pw reset):implement pw reset

--- src/com/pcl/lms/controller/ResetPasswordFromController.java
+++ src/com/pcl/lms/controller/ResetPasswordFromController.java
@@ -1,18 +1,29 @@
 package com.pcl.lms.controller;
 
+import com.pcl.lms.DB.Database;
+import com.pcl.lms.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ResetPasswordFromController {
     public AnchorPane context;
+    public Label lblCompany;
+    public Label lblVersion;
+    public PasswordField txtNewPassword;
+    public PasswordField txtConfirmPassword;
+    private  String email;
 
     public void SetUserData(String email){
-        System.out.println(email);
+            this.email=email;
     }
 
     public void navigateOtpVerificationForm(ActionEvent actionEvent) throws IOException {
@@ -20,7 +31,19 @@ public class ResetPasswordFromController {
     }
 
     public void resetPasswordOnAction(ActionEvent actionEvent) throws IOException {
-        setUi("LoginForm");
+        Optional<User> selectedUser = Database.userTable.stream().filter(u -> u.getEmail().equals(email)).findFirst();
+
+        if(selectedUser.isPresent()){
+            if (txtNewPassword.getText().trim().equals(txtConfirmPassword.getText().trim())){
+                selectedUser.get().setPassword(txtNewPassword.getText().trim());
+                new Alert(Alert.AlertType.INFORMATION,"Password Reset Successful").show();
+                System.out.println(selectedUser.get().getPassword());
+                setUi("LoginForm");
+
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Password doesn't mach").show();
+            }
+        }
     }
 
 
