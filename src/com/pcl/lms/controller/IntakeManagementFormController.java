6146e3508be9ec983ad8eb5c073511370f10fa4a
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-23 00:06:45 +0530

    feat(intake):implement save feat

--- src/com/pcl/lms/controller/IntakeManagementFormController.java
+++ src/com/pcl/lms/controller/IntakeManagementFormController.java
@@ -6,8 +6,15 @@ import com.pcl.lms.model.Programme;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class IntakeManagementFormController {
     public AnchorPane context;
@@ -15,7 +22,7 @@ public class IntakeManagementFormController {
     public Button btnSave;
     public DatePicker dteStart;
     public TextField txtName;
-    public ComboBox cmbProgram;
+    public ComboBox<String> cmbProgram;
     public TextField txtSearch;
     public TableView tblIntake;
     public TableColumn colId;
@@ -26,10 +33,10 @@ public class IntakeManagementFormController {
 
     public void initialize() {
         setIntakeId();
-        setTeacherData();
+        setProgrammeData();
     }
 
-    private void setTeacherData() {
+    private void setProgrammeData() {
         ObservableList<String> programsObList = FXCollections.observableArrayList();
         for (Programme temp:Database.programmeTable){
             programsObList.add(temp.getProgrammeId()+"-"+temp.getProgrammeName());
@@ -45,17 +52,41 @@ public class IntakeManagementFormController {
             String[] split = id.split("-");
             int lastDigit = Integer.parseInt(split[1]);
             lastDigit++;
-            txtId.setText("T-"+lastDigit);
+            txtId.setText("I-"+lastDigit);
         }
-        txtId.setText("T-1");
+        txtId.setText("I-1");
     }
 
     public void newIntakeOnAction(ActionEvent actionEvent) {
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
     }
 
     public void saveOnAction(ActionEvent actionEvent) {
+        if (btnSave.getText().equals("Save")) {
+            Database.intakeTable.add(new Intake(
+                   txtId.getText() ,
+                   Date.from(dteStart.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()) ,
+                    txtName.getText() ,
+                    cmbProgram.getValue()
+            ));
+            new Alert(Alert.AlertType.INFORMATION, "Saved").show();
+            setIntakeId();
+            setProgrammeData();
+            clearField();
+
+        }
+    }
+
+    private void clearField() {
+        txtName.clear();
+        dteStart.setValue(null);
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage =(Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/pcl/lms/view/"+location+".fxml"))));
     }
 }
