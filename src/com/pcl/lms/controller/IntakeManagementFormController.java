82af7f552040aa2376ba17aa953c953641292f54
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-22 23:29:26 +0530

    feat(intake):Create ui intake management form

---
+++ src/com/pcl/lms/controller/IntakeManagementFormController.java
@@ -0,0 +1,30 @@
+package com.pcl.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class IntakeManagementFormController {
+    public AnchorPane context;
+    public TextField txtId;
+    public Button btnSave;
+    public DatePicker dteStart;
+    public TextField txtName;
+    public ComboBox cmbProgram;
+    public TextField txtSearch;
+    public TableView tblIntake;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colDate;
+    public TableColumn colProgram;
+    public TableColumn colOption;
+
+    public void newIntakeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+}
