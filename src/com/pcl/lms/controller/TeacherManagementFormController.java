b3101231ff44099274aac0d4248f53866082ea95
Gihanvs98 <darkcreationsl98@gmail.com>
2025-10-27 23:36:58 +0530

    refactor(teacher):refactor settable data feat with search & delete feat

--- src/com/pcl/lms/controller/TeacherManagementFormController.java
+++ src/com/pcl/lms/controller/TeacherManagementFormController.java
@@ -2,6 +2,7 @@ package com.pcl.lms.controller;
 
 import com.pcl.lms.DB.Database;
 import com.pcl.lms.DB.DbConnection;
+import com.pcl.lms.model.Student;
 import com.pcl.lms.model.Teacher;
 import com.pcl.lms.view.tm.TeacherTm;
 import javafx.collections.FXCollections;
@@ -19,6 +20,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -70,37 +72,77 @@ public class TeacherManagementFormController {
     }
 
     private void setTeacherData(String searchText) {
-        ObservableList <TeacherTm> teacherObList = FXCollections.observableArrayList();
+        try{
+            ArrayList <Teacher> students =fetchTeachers(searchText);
+            ObservableList <TeacherTm> teacherObList = FXCollections.observableArrayList();
 
-        for (Teacher teacher:Database.teacherTable){
-            if(teacher.getName().toLowerCase().contains(searchText.toLowerCase())){
-                Button btn = new Button("Delete");
-                TeacherTm teacherTm = new TeacherTm(
-                        teacher.getId(),
-                        teacher.getName(),
-                        teacher.getAddress(),
-                        teacher.getContact(),
-                        btn
-                );
-                btn.setOnAction((event) -> {
-                    Alert alert=new Alert
-                            (Alert.AlertType.CONFIRMATION,"Are you sure you want to delete this teacher?"
-                                    ,ButtonType.YES,ButtonType.NO);
-                    alert.showAndWait();
-                    if(alert.getResult()==ButtonType.YES){
-                        Database.teacherTable.remove(teacher);
-                        setTeacherData(searchText);
-                        setTeacherId();
-                        new Alert(Alert.AlertType.INFORMATION,"Deleted Successfully").show();
-                    }
-                });
-                teacherObList.add(teacherTm);
+            for (Teacher teacher:students){
+                    Button btn = new Button("Delete");
+                    TeacherTm teacherTm = new TeacherTm(
+                            teacher.getId(),
+                            teacher.getName(),
+                            teacher.getAddress(),
+                            teacher.getContact(),
+                            btn
+                    );
+                    btn.setOnAction((event) -> {
+                        Alert alert=new Alert
+                                (Alert.AlertType.CONFIRMATION,"Are you sure you want to delete this teacher?"
+                                        ,ButtonType.YES,ButtonType.NO);
+                        alert.showAndWait();
+                        if(alert.getResult()==ButtonType.YES){
+                            try{
+                                boolean isDeleted=deleteTeacher(teacherTm.getId());
+                                if (isDeleted){
+                                    setTeacherData(searchText);
+                                    setTeacherId();
+
+                                    return;
+                                }
+                                new Alert(Alert.AlertType.WARNING,"some thing went wrong").show();
+
+                            }catch(ClassNotFoundException | SQLException e){
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+                    teacherObList.add(teacherTm);
+
+
             }
-
-
+            tblTeacher.setItems(teacherObList);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
 
         }
-        tblTeacher.setItems(teacherObList);
+
+    }
+
+    private boolean deleteTeacher(String id) throws SQLException, ClassNotFoundException {
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement ps = connection.prepareStatement("DELETE FROM teacher WHERE id=?");
+
+        ps.setString(1,id);
+        return ps.executeUpdate()>0;
+    }
+
+    private ArrayList<Teacher> fetchTeachers(String searchText) throws SQLException, ClassNotFoundException {
+       ArrayList<Teacher> teachers = new ArrayList<>();
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement ps = connection.prepareStatement("SELECT * FROM teacher WHERE name LIKE ?");
+        ps.setString(1,"%"+searchText+"%");
+        ResultSet set = ps.executeQuery();
+        while(set.next()){
+            teachers.add(new Teacher
+                    (set.getString(2),
+                      set.getString(1),
+                       set.getString(3),
+                            set.getString(4))
+                    );
+        }
+        return teachers;
     }
 
     private void setTeacherId() {
